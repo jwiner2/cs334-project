f8a11e1b4b9e0452e16dfa0835bbcb16e01ec8c8
Jwiner2 <jwiner2@emory.edu>
2020-11-17 12:49:53 -0500

    creating Args Parser models.py

--- models.py
+++ models.py
@@ -96,52 +96,52 @@ def main():
     """
     Main file to run from the command line.
     """
-    # set up the program to take in arguments from the command line
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--vectorF",
-                        help="filename for features of the training data",
-                        default="data/vectorized_data.csv") #todo: need to change to correct file
-    args = parser.parse_args()
-    xFull = pd.read_csv(args.vectorF)
-    yFull = xFull["price"]
-    xFull= xFull.drop("price", axis=1)
-
-    xTrain,xTest,yTrain,yTest= train_test_split( xFull, yFull, test_size=0.33, random_state=42)
-
-    xTrain.to_csv("data/xTrain.csv", index=False)
-    yTrain.to_csv("data/yTrain.csv", index=False)
-    xTest.to_csv("data/xTest.csv", index=False)
-    yTest.to_csv("data/yTest.csv", index=False)
-
-
-
-
-    # parser.add_argument("--xTrain",
+    ##### Used to set up data files
+    # # set up the program to take in arguments from the command line
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("--vectorF",
     #                     help="filename for features of the training data",
-    #                     default="q4xTrain.csv") #todo: need to change to correct file
-    # parser.add_argument("--yTrain",
-    #                     help="filename for labels associated with training data",
-    #                     default="q4yTrain.csv")#todo: need to change to correct file
-    # parser.add_argument("--xTest",
-    #                     help="filename for features of the test data",
-    #                     default="q4xTest.csv")#todo: need to change to correct file
-    # parser.add_argument("--yTest",
-    #                     help="filename for labels associated with the test data",
-    #                     default="q4yTest.csv")#todo: need to change to correct file
-    #
+    #                     default="data/vectorized_data.csv") #todo: need to change to correct file
     # args = parser.parse_args()
-    # # load the train and test data assumes you'll use numpy
-    # xTrain = file_to_numpy(args.xTrain)
-    # yTrain = file_to_numpy(args.yTrain)
-    # xTest = file_to_numpy(args.xTest)
-    # yTest = file_to_numpy(args.yTest)
+    # xFull = pd.read_csv(args.vectorF)
+    # yFull = xFull["price"]
+    # xFull= xFull.drop("price", axis=1)
     #
+    # xTrain,xTest,yTrain,yTest= train_test_split( xFull, yFull, test_size=0.33, random_state=42)
     #
-    # #run pca on data
+    # xTrain.to_csv("data/xTrain.csv", index=False)
+    # yTrain.to_csv("data/yTrain.csv", index=False)
+    # xTest.to_csv("data/xTest.csv", index=False)
+    # yTest.to_csv("data/yTest.csv", index=False)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--xTrain",
+                        help="filename for features of the training data",
+                        default="data/xTrain.csv") #todo: need to change to correct file
+    parser.add_argument("--yTrain",
+                        help="filename for labels associated with training data",
+                        default="data/yTrain.csv")#todo: need to change to correct file
+    parser.add_argument("--xTest",
+                        help="filename for features of the test data",
+                        default="data/xTest.csv")#todo: need to change to correct file
+    parser.add_argument("--yTest",
+                        help="filename for labels associated with the test data",
+                        default="data/yTest.csv")#todo: need to change to correct file
+
+    args = parser.parse_args()
+    # load the train and test data assumes you'll use numpy
+    xTrain = file_to_numpy(args.xTrain)
+    yTrain = file_to_numpy(args.yTrain)
+    xTest = file_to_numpy(args.xTest)
+    yTest = file_to_numpy(args.yTest)
+
+
+    #the linear regression as well as PCA are having a hard time working with the catagorical data
+    #run pca on data
     # xTrainPCA, xTestPCA = pcaCreate(xTrain,yTrain,xTest,yTest)
-    #
+
     # #run Linear regression
-    # yHatLR,scoreLR=linearRegressionModel(xTrainPCA, yTrain,xTestPCA,yTest)
+    # yHatLR,scoreLR=linearRegressionModel(xTrain, yTrain,xTest,yTest)
     # #run Linear Regression Ridge
     # yHatLRRidge,scoreLRRidge=linearRegressionRidgeModel(xTrainPCA, yTrain,xTestPCA,yTest)
 
